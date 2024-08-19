908eafe2e8037eb118ea63cd7268cacc24912263
ressiwage <ressiwage@ya.ru>
2024-08-19 16:16:03 +0500

    transfer and prettify

---
+++ web/setupArchivesMenu.js
@@ -0,0 +1,71 @@
+const PLEASE_LOGIN = document.getElementById('please-login');
+const ARCHIVE_SECTION = document.getElementById('archives');
+const VIDEO_SECTION = document.getElementById('videos');
+const ARCHIVES_CONTAINER = document.getElementById('archives-container');
+const VIDEO_CONTAINER = document.getElementById('videos-container');
+
+async function loadArchives() {
+    let token = JSON.parse(localStorage.getItem("comexp-token"));
+    if (token === null) { return }
+    ARCHIVE_SECTION.classList.remove('d-none');
+    PLEASE_LOGIN.classList.add('d-none')
+    let response = await fetch('http://127.0.0.1:5000/get_archives', {
+        headers: { 'archives-token': token }
+    });
+    if (response.ok) {
+        const archives = [];
+        const archives_js = (await response.json())['archives'];
+        console.log(archives_js);
+        for (let archive of archives_js) {
+            const a_div = document.createElement('div');
+            a_div.className = 'w700 border border-dark mx-1 c-pointer minimal-hover'
+            const a_title = document.createElement('h1');
+            a_title.innerHTML = `архив ${archive.name}`
+            const a_par = document.createElement('p');
+            a_par.innerHTML = `ид: ${archive.id}`;
+            [a_title, a_par].forEach((e) => a_div.appendChild(e));
+
+            a_div.addEventListener('click', () => {
+                loadVideos(archive.videos);
+                VIDEO_SECTION.classList.remove('d-none');
+                document.getElementById('archive-id').value = archive.id;
+            })
+
+            archives.push(
+                a_div
+            )
+        }
+        for (const archive of archives) {
+            ARCHIVES_CONTAINER.appendChild(archive)
+        }
+
+    } else {
+        console.log('not ok')
+    }
+}
+async function loadVideos(videos_json) {
+    const videos = [];
+    VIDEO_CONTAINER.innerHTML = '';
+
+    for (let video of videos_json) {
+        const a_div = document.createElement('div');
+        a_div.className = 'w700 border border-dark mx-1 minimal-hover'
+        const a_title = document.createElement('h1');
+        a_title.className = 'text-wrap text-break'
+        a_title.innerHTML = `видео ${video.name}`
+        const a_par = document.createElement('p');
+        a_par.innerHTML = `путь: ${video.path}`;
+        [a_title, a_par].forEach((e) => a_div.appendChild(e));
+
+
+        videos.push(
+            a_div
+        )
+    }
+    for (const video of videos) {
+        VIDEO_CONTAINER.appendChild(video)
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => { await loadArchives() })
---
+++ web/setupForms.js
@@ -0,0 +1,108 @@
+function handle_login_js(json) {
+    localStorage.setItem('comexp-token', JSON.stringify(json['token']));
+    location.reload();
+}
+
+//настраиваем логин
+if (document.forms['loginform'] !== undefined) {
+    document.forms['loginform'].addEventListener('submit', (event) => {
+        event.preventDefault();
+        fetch(event.target.action, {
+            method: 'POST',
+            body: new URLSearchParams(new FormData(event.target)), // event.target is the form
+        }).then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json(); // or response.text() or whatever the server sends
+        }).then((json) => {
+            handle_login_js(json);
+        }).catch((error) => {
+            console.log(error);
+        });
+    });
+}
+
+//настраиваем потоковую загрузку видео
+if (document.forms['upload-video-form'] !== undefined) {
+    document.forms['upload-video-form'].addEventListener('submit', (event) => {
+        event.preventDefault();
+        let token = JSON.parse(localStorage.getItem("comexp-token"));
+        var fd = new FormData();
+        fd.append('file', document.getElementById('upload-file').files[0])
+        fetch(event.target.action, {
+            method: 'POST',
+            body: document.getElementById('upload-file').files[0], // event.target is the form
+            headers: {
+                'archives-token': token,
+                'md5': document.getElementById('md5').value,
+                'archive-id': document.getElementById('archive-id').value,
+                'filename': document.getElementById('upload-file').files[0].name
+            }
+        }).then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json(); // or response.text() or whatever the server sends
+        }).then((json) => {
+            // handle_login_js(json);
+        }).catch((error) => {
+            console.log(error);
+        });
+    });
+}
+
+
+//md5 calculation
+const chunkSize = 64 * 1024 * 1024;
+const fileReader = new FileReader();
+let hasher = null;
+
+function hashChunk(chunk) {
+    return new Promise((resolve, reject) => {
+        fileReader.onload = async (e) => {
+            const view = new Uint8Array(e.target.result);
+            hasher.update(view);
+            resolve();
+        };
+
+        fileReader.readAsArrayBuffer(chunk);
+    });
+}
+
+const readFile = async (file) => {
+    if (hasher) {
+        hasher.init();
+    } else {
+        hasher = await hashwasm.createMD5();
+    }
+
+    const chunkNumber = Math.floor(file.size / chunkSize);
+
+    for (let i = 0; i <= chunkNumber; i++) {
+        const chunk = file.slice(
+            chunkSize * i,
+            Math.min(chunkSize * (i + 1), file.size)
+        );
+        await hashChunk(chunk);
+    }
+
+    const hash = hasher.digest();
+    return Promise.resolve(hash);
+};
+
+
+//вычисляем md5 хэш файла при его загрузке
+if (document.getElementById('upload-file') !== null) {
+    document.getElementById('upload-file').addEventListener("change", async (event) => {
+        const file = event.target.files[0];
+        const uploadBtn = document.getElementById("submit-upload")
+        uploadBtn.disabled = true;
+        const oldValue = uploadBtn.value;
+        uploadBtn.value = "вычисляется хэш...";
+        const hash = await readFile(file);
+        uploadBtn.disabled = false;
+        uploadBtn.value = oldValue;
+        document.getElementById("md5").value = hash;
+    });
+}
