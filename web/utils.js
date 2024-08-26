3fee1c6aa25782d0f03c81579252f1a2ead76616
ilya notebook <ressivage@ya.ru>
2024-08-26 14:03:41 +0500

    delete archive

---
+++ web/utils.js
@@ -0,0 +1,10 @@
+function deleteArchive(url, id) {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({'archive_id':id}),
+        headers: {
+            'archives-token': JSON.parse(localStorage.getItem('comexp-token'))
+        }
+    }
+    )
+}
